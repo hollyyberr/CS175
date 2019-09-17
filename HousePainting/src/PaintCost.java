ebbaa40c7a574edf2dc2db81d91335a3c3936fbf
Holly Bernich <s1299693@monmouth.edu>
2019-09-16 20:56:58 -0400

    first code for house painting

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,32 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 * This program is going to calculate the total cost to paint a house.
+		 */
+		double houseLengthFeet = 0;
+		double houseWidthFeet = 0;
+		double houseHeightFeet = 0;
+		double houseLengthInches = 0;
+		double houseWidthInches = 0;
+		double houseHieghtInches = 0;
+		double windowLengthFeet = 0;
+		double windowLengthInches = 0;
+		double windowHeightInches = 0;
+		double windowHeightFeet = 0;
+		int windowNumber = 0;
+		double doorLengthFeet = 0;
+		double doorHeightFeet = 0;
+		double doorLengthInches = 0;
+		double doorHeightInches = 0;
+		int doorNumber = 0;
+		double sqFootCost = 0;
+		double sqFootPeak = 0;
+		double sqFootWindows = 0;
+		double sqFootDoors = 0;
+
+
+	}
+
+}
