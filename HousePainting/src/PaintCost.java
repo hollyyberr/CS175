f961305d79bb5f963ef8c2b353c0fc71c0d665b5
Holly Bernich <s1299693@monmouth.edu>
2019-09-23 20:43:25 -0400

    Finalized House Painting Program

--- HousePainting/src/PaintCost.java
+++ HousePainting/src/PaintCost.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class PaintCost {
@@ -24,11 +25,18 @@ public class PaintCost {
 		int doorNumber = 0;
 		double sqFootCost = 0;
 		double sqFootPeak = 0;
+		double sqFootNormal = 0;
 		double sqFootWindows = 0;
 		double sqFootDoors = 0;
-		
+		double totalWindows = 0; 
+		double totalDoors = 0;
+		double sqFootTotalSides = 0;
+		double totalSubtractedFootage = 0;
+		double sqFootEndingTotal = 0;
+		double endingCost = 0;
 		//Scan in numbers for code
 		Scanner in = new Scanner(System.in);
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
 		
 		//Enter the length of the house in feet
 		System.out.print("Enter the whole length in feet of the house without the remainder of inches:  ");
@@ -97,7 +105,37 @@ public class PaintCost {
 		sqFootCost = in.nextDouble();
 		
 		//Calculate how many square feet are in the peak sides
-		sqFootPeak = (houseLengthFeet * houseWidthFeet * (1/2) * (houseLengthFeet * (houseLengthFeet - houseWidthFeet)));
+		sqFootPeak = (houseLengthFeet * houseWidthFeet) + (.5 * (houseLengthFeet * (houseHeightFeet - houseWidthFeet)));
+		
+		//Calculate how many square feet are in a normal side
+		sqFootNormal = (houseWidthFeet * houseLengthFeet);
+		
+		//Calculate the square footage of the windows
+		sqFootWindows = (windowHeightFeet * windowLengthFeet);
+		
+		//Multiply the number of windows by the square footage of the windows
+		totalWindows = (sqFootWindows * windowNumber);
+		
+		//Calculate the square footage of the doors
+		sqFootDoors = (doorHeightFeet * doorLengthFeet);
+		
+		//Multiply the square footage by the doors by the number of doors
+		totalDoors = (sqFootDoors * doorNumber);
+		
+		//Add the total square footage of the normal sides to the peak sides(multiply by 2 because there is 2 sides of each)
+		sqFootTotalSides = (2 * sqFootNormal) + (2 * sqFootPeak);
+		
+		//Add the square footage of windows to the square footage of doors
+		totalSubtractedFootage = (totalDoors + totalWindows);
+		
+		//Subtract the square footage of the windows and doors from the total square footage of the house
+		sqFootEndingTotal = (sqFootTotalSides - totalSubtractedFootage);
+		
+		//Multiply this ending square footage with the cost per hour
+		endingCost = (sqFootEndingTotal * sqFootCost);
+		
+		//Print out the total cost for painting
+		System.out.println("The total cost to paint the house is " + formatter.format(endingCost) + ".");
 		
 		
 		
@@ -107,15 +145,8 @@ public class PaintCost {
 		
 /*
  * 
-Calculate how many square feet are in the peak sides (L*W + 1/2(L*(H-W))
-Calculate how many square feet are in a normal side (L*W)
-Calculate the square footage of the windows (L*W)
-Multiply the number of windows by the square footage of the windows.
-Calculate the square footage of the doors (L*W)
-Multiply the square footage of the doors by the number of doors.
-Add the total square footage of the normal sides to the peak sides.
-Subtract the square footage of the windows and doors from the total square footage of the house.
-Multiply this ending square footage number with the cost per hour.
+
+
 Print out the total cost for painting.
 
  */
