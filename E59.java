e4cb0ae6881118b5698b58791ebc2d1be52134d1
hollyyberr <55116533+hollyyberr@users.noreply.github.com>
2019-10-30 18:35:45 -0400

    Add files via upload

--- E59.java
+++ E59.java
@@ -6,41 +6,51 @@ public class E59 {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter an angle value of the direction you are heading: ");
 		double angle = scan.nextDouble();
+		double angle2 = 0;
+		double angle3 = 22.5;
+		double angle4 = 67.5;
+		double angle5 = 112.5;
+		double angle6 = 157.5;
+		double angle7 = 202.5;
+		double angle8 = 247.5;
+		double angle9 = 292.5;
+		double angle10 = 337.5;
+		double angle11 = 360.5;
+		
 
-
-		if (angle > 0 && angle <= 22.5)
+		if (angle > angle2 && angle <= angle3)
 		{
 			System.out.println("north (N)");
 		}
-		else if(angle > 22.5 && angle < 67.5)
+		else if(angle > angle3 && angle < angle4)
 		{
 			System.out.println("northeast (NE) ");
 		}
-		else if(angle >= 67.5 && angle <= 112.5)
+		else if(angle >= angle4 && angle <= angle5)
 		{
 			System.out.println("east (E) ");
 		}
-		else if(angle > 112.5 && angle < 157.5)
+		else if(angle > angle5 && angle < angle6)
 		{
 			System.out.println("southeast (SE) ");
 		}
-		else if(angle>=157.5 && angle<=202.5)
+		else if(angle >= angle6 && angle <= angle7)
 		{
 			System.out.println("south (S)");
 		}
-		else if(angle>202.5 && angle<247.5)
+		else if(angle > angle7 && angle < angle8)
 		{
 			System.out.println("southwest (SW)");
 		}
-		else if(angle>=247.5 && angle<=292.5)
+		else if(angle >= angle8 && angle <= angle9)
 		{
 			System.out.println("west (W)");
 		}
-		else if(angle > 292.5 && angle < 337.5)
+		else if(angle > angle9 && angle < angle10)
 		{
 			System.out.println("northwest (NW)");
 		}
-		else if(angle >= 337.5 && angle <= 360.5)
+		else if(angle >= angle10 && angle <= angle11)
 		{
 			System.out.println("north (N)");
 		}
