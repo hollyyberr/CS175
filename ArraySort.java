c635a68023f085f85c48f93afb9094ae6c3dc149
hollyyberr <55116533+hollyyberr@users.noreply.github.com>
2019-11-18 20:32:48 -0500

    nested for loops sorting array

---
+++ ArraySort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+	public static void main(String[] args) {
+		int lowest;
+		String printedNumbers;
+		double[] numbers = {88,47,90,50,77,83,92,96,62,79};
+		double[] numberSort = new double[10];
+
+		for (int x = 0; x < numbers.length; x++) {
+			for (int y = x; y < numbers.length; y++) {
+				if (numbers[x] > numbers[y]) {
+					lowest = (int) numbers[x];
+					numbers[x] = numbers[y];
+					numbers[y] = lowest;
+				}
+			}
+		}
+		
+		printedNumbers = Arrays.toString(numbers);
+		System.out.println(printedNumbers);
+	}
+}
+
+
+
+
